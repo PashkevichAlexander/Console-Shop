9e3f8a89c8ea10f1a7503cb8f1328c6fabeb133e
Alexander Pashkevich <pashkevich.a.a@mail.ru>
2021-12-26 22:17:27 +0300

    new commit

--- src/main/java/task/app/App.java
+++ src/main/java/task/app/App.java
@@ -1,14 +1,26 @@
 package task.app;
 
+import task.service.Reader;
+
 public class App {
     private static final String menu = """
-            1 - CreatCatalog
             2 - AddCategoryInCatalog
             3 - AddItemInCategory
             4 - PrintCatalog
             """;
     public void run(){
+        while (true){
+            Reader reader = new Reader();
+            int num = reader.readInt();
+            switch (num){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
 
-
+            }
+        }
     }
 }
